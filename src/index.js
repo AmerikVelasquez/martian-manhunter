1a0b42c9e960ae66a3db8c35e1be75b8d86db088
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-08-08 14:40:43 -0700

    allow the button to make the api call
    Co-authored-by: Aaron Kauffman <aaron.christian.kauffman@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Bored from './bored.js';
+import Random from './random.js';
+// import Type from './type.js';
+
+async function makeApiCall() {
+    //case random:
+    const response = await Random.getRandom();
+    // getElements(response);
+    console.log(response)
+  }
+
+//   function getElements(response){
+//   }
+
+$(document).ready(function(){
+  $('button').click(function(){
+    makeApiCall();
+  })
+});
 
